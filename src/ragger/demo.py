8aec7870f13e02bc5918e1896ba53a942c1b4923
Anders Jess Pedersen <anders.j.pedersen@alexandra.dk>
2024-04-24 08:40:42 +0200

    feat: add gradio interface demo

--- src/ragger/demo.py
+++ src/ragger/demo.py
@@ -1,5 +1,6 @@
 """A Gradio demo of the RAG system."""
 
+import gradio as gr
 from omegaconf import DictConfig
 
 from .rag_system import RagSystem
@@ -20,4 +21,30 @@ class Demo:
 
     def launch(self) -> None:
         """Launch the demo."""
-        pass
+
+        def generate(query: str) -> str:
+            """Generate an answer to a query.
+
+            Args:
+                query:
+                    The query to generate an answer to.
+
+            Returns:
+                The generated answer.
+            """
+            answer, documents = self.rag_system.answer(query)
+            sources_quote = "'".join(document.text for document in documents)
+            sources_ids_str = ", ".join(document.id for document in documents)
+            return (
+                f"Model answer:\n\t {answer},\n\n"
+                f"Based on the following text:\n\t '{sources_quote}',\n\n"
+                f"Source for the text:\n\t {sources_ids_str}"
+            )
+
+        gr.Interface(
+            fn=generate,
+            inputs=gr.Textbox(lines=2, label="Query"),
+            outputs=gr.Textbox(label="Answer"),
+            title="RAG System",
+            description="A demo of the RAG system.",
+        ).launch()
