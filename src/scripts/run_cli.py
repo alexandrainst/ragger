f1e1ce55191d10a326509c3484c0eab7080f6666
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2024-04-30 16:38:47 +0200

    fix: Rename config to `ragger_config`

--- src/scripts/run_cli.py
+++ src/scripts/run_cli.py
@@ -15,7 +15,7 @@ from ragger.utils import Document, format_answer
 load_dotenv()
 
 
-@hydra.main(config_path="../../config", config_name="config", version_base=None)
+@hydra.main(config_path="../../config", config_name="ragger_config", version_base=None)
 def main(config: DictConfig) -> None:
     """Run the CLI.
 
