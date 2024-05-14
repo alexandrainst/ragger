9cecd3505dae796424845bc49ab2bdf1744ab757
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2024-05-14 19:29:29 +0200

    chore: Use filenames in configs instead of including paths

--- src/ragger/document_store.py
+++ src/ragger/document_store.py
@@ -55,7 +55,11 @@ class JsonlDocumentStore(DocumentStore):
                 The Hydra configuration.
         """
         super().__init__(config)
-        data_path = Path(self.config.dirs.data) / self.config.document_store.filename
+        data_path = (
+            Path(self.config.dirs.data)
+            / self.config.dirs.processed
+            / self.config.document_store.filename
+        )
         data_dicts = [
             json.loads(line)
             for line in data_path.read_text().splitlines()
