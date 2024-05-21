194ea49921c0fa7bc4a27e3338a30a650b6dd39a
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2024-05-21 14:18:08 +0200

    fix: Try enforce_eager

--- src/ragger/generator.py
+++ src/ragger/generator.py
@@ -203,6 +203,7 @@ class VLLMGenerator(Generator):
             seed=config.random_seed,
             tensor_parallel_size=torch.cuda.device_count(),
             disable_custom_all_reduce=True,
+            enforce_eager=True,
         )
         self.tokenizer = self.model.get_tokenizer()
         self.logits_processor = JSONLogitsProcessor(
