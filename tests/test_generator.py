e3e40a4f33b0694021c3cf85e4cbf1341fda4b45
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2024-05-21 15:15:14 +0200

    debug

--- tests/test_generator.py
+++ tests/test_generator.py
@@ -93,7 +93,6 @@ class TestVllmGenerator:
         """Test that the generator is initialised correctly."""
         generator = VllmGenerator(config=config)
         assert generator
-        breakpoint()
         del generator
 
     def test_generate(self, config, query, documents) -> None:
