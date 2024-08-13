e20f892fc4e7d5c79d1be3f7460f25cd1f0e8959
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2024-08-13 10:30:54 +0200

    chore: Import more in the global namespace

--- src/ragger/__init__.py
+++ src/ragger/__init__.py
@@ -1,5 +1,13 @@
 """A general-purpose RAG system and demo application."""
 
-from .data_models import Document, Embedding
+from .data_models import (
+    Document,
+    DocumentStore,
+    Embedder,
+    Embedding,
+    EmbeddingStore,
+    Generator,
+    Index,
+)
 from .demo import Demo
 from .rag_system import RagSystem
