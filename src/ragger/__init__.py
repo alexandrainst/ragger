34a59953ec04acdb774aa9cdc59b2c1aed12f09a
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2024-08-13 10:45:16 +0200

    chore: Add logging config and __version__ to __init__

--- src/ragger/__init__.py
+++ src/ragger/__init__.py
@@ -1,5 +1,8 @@
 """A general-purpose RAG system and demo application."""
 
+import importlib.metadata
+import logging
+
 from .data_models import (
     Document,
     DocumentStore,
@@ -11,3 +14,12 @@ from .data_models import (
 )
 from .demo import Demo
 from .rag_system import RagSystem
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s ⋅ %(name)s ⋅ %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+
+# Fetches the version of the package as defined in pyproject.toml
+__version__ = importlib.metadata.version(__package__)
