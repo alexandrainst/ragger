745646f1c01edea29df341f3cfb1a0db58136ff5
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2024-08-13 15:50:18 +0200

    tests: Add some more demo tests

--- tests/test_demo.py
+++ tests/test_demo.py
@@ -5,17 +5,27 @@ import typing
 from pathlib import Path
 from tempfile import NamedTemporaryFile
 
+import gradio as gr
+import pytest
 from ragger.demo import Demo
 
 
-def test_initialisation_no_feedback(rag_system):
-    """Test the initialisation of the demo."""
+@pytest.fixture(scope="module")
+def demo(rag_system) -> typing.Generator[Demo, None, None]:
+    """A RAG demo for testing."""
     with NamedTemporaryFile(mode="w", suffix=".db") as file:
-        Demo(rag_system=rag_system, feedback_db_path=Path(file.name))
+        demo = Demo(rag_system=rag_system, feedback_db_path=Path(file.name))
+        yield demo
+        demo.close()
         file.close()
 
 
-def test_initialisation_feedback(rag_system):
+def test_initialisation(demo):
+    """Test the initialisation of the demo."""
+    assert isinstance(demo, Demo)
+
+
+def test_initialisation_with_feedback(rag_system):
     """Test the initialisation of the demo."""
     feedback_modes: list[typing.Literal["strict-feedback", "feedback"]] = [
         "strict-feedback",
@@ -32,3 +42,9 @@ def test_initialisation_feedback(rag_system):
             with sqlite3.connect(database=demo.feedback_db_path) as connection:
                 assert connection.execute(sql).fetchone()
             file.close()
+
+
+def test_build(demo):
+    """Test that the demo can be built."""
+    blocks = demo.build_demo()
+    assert isinstance(blocks, gr.Blocks)
