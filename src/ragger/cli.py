faa4420aa30971fe7b827cf3bd978719c1e327b0
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2024-08-16 20:54:49 +0200

    feat: Allow JSON config

--- src/ragger/cli.py
+++ src/ragger/cli.py
@@ -1,5 +1,6 @@
 """Command-line interface for the `ragger` package."""
 
+import json
 from pathlib import Path
 
 import click
@@ -18,8 +19,17 @@ def run_demo(config_file: str) -> None:
         config_file:
             Path to the configuration file.
     """
-    with Path(config_file).open("r") as f:
-        config = yaml.safe_load(f)
+    config_path = Path(config_file)
+    with config_path.open() as f:
+        if config_path.suffix == ".json":
+            config = json.load(f)
+        elif config_path.suffix == ".yaml":
+            config = yaml.safe_load(f)
+        else:
+            raise ValueError(
+                f"Unsupported file extension: {config_path.suffix}. Please provide a "
+                "JSON or YAML file."
+            )
 
     rag_system = RagSystem.from_config(config=config)
     demo = Demo.from_config(rag_system=rag_system, config=config)
