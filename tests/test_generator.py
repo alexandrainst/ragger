d34d26d7bb8cfcf3f18b71633c64a2cbf69540fe
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2024-08-21 11:29:45 +0200

    tests: Skip tests if generator can't be initialised

--- tests/test_generator.py
+++ tests/test_generator.py
@@ -5,6 +5,7 @@ import typing
 import pytest
 import ragger.generator
 from ragger.data_models import GeneratedAnswer
+from ragger.exceptions import MissingPackage
 from ragger.generator import Generator
 
 
@@ -24,7 +25,7 @@ def generator(
         generator_cls = request.param
         generator = generator_cls(**special_kwargs.get(generator_cls.__name__, {}))
         yield generator
-    except (ImportError, RuntimeError):
+    except MissingPackage:
         pytest.skip("The generator could not be imported.")
 
 
